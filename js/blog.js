24ee110a7bc258ce466352b10b683f78b07b8898
JSON <838432498@qq.com>
2016-08-16 19:02:23 +0800

    fix scroll bug

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,12 @@
 (function(){
 	//先把默认的markdown生成的列表隐藏。
-var md = document.querySelector('#markdown-toc-section');
+var md = document.querySelector('#markdown-toc-section'),
+nav = document.querySelector('header .nav'),
+catalog = document.querySelector('.catalog');
 md.style.display = 'none';
+catalog.style.top = nav.offsetTop + nav.offsetHeight + 10 + 'px';
+window.onresize = function(){
+	catalog.style.top = nav.offsetTop + nav.offsetHeight + 10 + 'px';
+};
 
-}());
+ }());
--- js/post.js
+++ js/post.js
@@ -1,9 +1,15 @@
 (function(){
 	//先把默认的markdown生成的列表隐藏。
-var mdc = document.querySelector('#markdown-toc'),
+var body = document.querySelector('body'),
+mdc = document.querySelector('#markdown-toc'),
 catalog = document.querySelector('.catalog'),
 article = document.querySelector('article'),
-begin, end;
+h2 = document.querySelector('header h2'),
+begin, end, speed;
+catalog.style.top = h2.offsetTop + h2.offsetHeight + 10 + 'px';
+window.onresize = function(){
+	catalog.style.top = h2.offsetTop + h2.offsetHeight + 10 + 'px';
+};
 
 mdc.style.display = 'none';
 
@@ -15,27 +21,40 @@ if(mdc === null){
 	var mdcContent = mdc.innerHTML;
 	catalog.insertAdjacentHTML('afterbegin', mdcContent);
 	mdc.parentNode.removeChild(mdc);
-	catalog.onclick = function(e){
-		begin = article.scrollTop;
-		e.preventDefault();
-		var catId = e.target.id.substring(13);
-		var realId = article.querySelector('#' + catId);
-		end = realId.offsetTop - 20;
-		move(article, begin, end);
-	};
+	//平滑滚动
+	if(body.offsetWidth > 960){
+		catalog.onclick = function(e){
+			 try{
+				 begin = article.scrollTop;
+ 				e.preventDefault();
+ 				var catId = e.target.id.substring(13);
+ 				var realId = article.querySelector('#' + catId);
+ 				end = realId.offsetTop - 20;
+ 				if(article.scrollHeight-article.offsetHeight < end){
+ 					end = article.scrollHeight-article.offsetHeight;
+ 				}
+ 				move(article, begin, end);
+			} catch(ex){
+				//接住点击catalog非链接部分抛出的错误
+				return;
+			}
+
+		};
+	}
 
 }
 
 //运动函数
 function move(obj, begin, end){
 	clearInterval(obj.timer);
- obj.timer = setInterval(function(){
-		var speed = (end - article.scrollTop)/10;
+	obj.timer = setInterval(function(){
+	 	speed = (end - article.scrollTop)/6;
 		speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
 		article.scrollTop += speed;
 		if(article.scrollTop === end){
 			clearInterval(obj.timer);
 		}
+		//没有考虑最后一个标题距离不够的bug
 
 	},30);
 }
